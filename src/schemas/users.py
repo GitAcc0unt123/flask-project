8af99fa8f12653f73c4f7a722d5e6369d754746e
name <example@example.com>
2023-03-01 10:45:46 +0300

    change regex

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -2,19 +2,19 @@ from marshmallow import Schema, fields, validate
 
 class SignInSchema(Schema):
     username = fields.Str(required=True, validate=[
-        validate.Regexp('^[A-Za-z]\w{2,254}$')])
+        validate.Regexp(r'^[A-Za-z]\w{2,254}$')])
     password = fields.Str(required=True, load_only=True, validate=[
-        validate.Regexp('^[a-zA-Z0-9]{9,255}$')
+        validate.Regexp(r'^[a-zA-Z0-9]{9,255}$')
     ])
 
 class SignUpSchema(Schema):
     username = fields.Str(required=True, validate=[
-        validate.Regexp('^[A-Za-z]\w{2,254}$')])
+        validate.Regexp(r'^[A-Za-z]\w{2,254}$')])
     password = fields.Str(required=True, load_only=True, validate=[
-        validate.Regexp('^[a-zA-Z0-9]{9,255}$')
+        validate.Regexp(r'^[a-zA-Z0-9]{9,255}$')
     ])
     name = fields.Str(required=True, validate=[
         validate.Length(min=1, max=255),
-        validate.Regexp('^[a-zA-Z+ ]*[a-zA-Z]*$')
+        validate.Regexp(r'^[a-zA-Z]+( [a-zA-Z]+)*$')
     ])
     email = fields.Email(required=True)
